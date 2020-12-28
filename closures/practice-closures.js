a5c5752ad4aa87335afbd5cc78c046bc2ee6749a
abradley2000 <aaron.s.bradley2000@gmail.com>
2020-12-28 02:58:25 -0600

    Compled classes and closures

--- closures/practice-closures.js
+++ closures/practice-closures.js
@@ -10,11 +10,21 @@
   
   //CODE HERE
 
+  function myFunc() {
+    let myStr = 'super secret string'
+    function getSecretString() {
+      return myStr
+    }
+    return getSecretString()
+  }
   
   //Now create a variable called secretString. Its value should be the invocation of myFunc.
 
   //CODE HERE
   
+  let secretString = myFunc()
+  console.log(secretString)
+  // ??? It contains the correct string ???
   
   ////////////PROBLEM 2////////////
 
@@ -30,28 +40,45 @@
   
   //CODE HERE
 
+  function lightSwitch() {
+    let isTheLightOn = false
+    function flipTheSwitch() {
+      isTheLightOn = !isTheLightOn;
+      if (isTheLightOn = true) {
+        return 'The light is on'
+      }
+      else {
+        return 'The light is off'
+      }
+    }
+    return flipTheSwitch();
+  }
   
   //Create a variable called kitchenSwitch whose value is the invocation of lightSwitch.
   
   //CODE HERE
 
+  let kitchenSwitch = lightSwitch()
   
   //Invoke kitchenSwitch.
 
   //CODE HERE
   
+  kitchenSwitch()
   
   //Create a variable called bathroomSwitch whose value is the invocation of lightSwitch. 
 
   //CODE HERE
   
+  let bathroomSwitch = lightSwitch()
   
   //Invoke bathroomSwitch twice.
   
   //CODE HERE
 
-  
-  
+  bathroomSwitch()
+  bathroomSwitch()
+
   ////////////PROBLEM 3////////////
 
   /*
@@ -65,6 +92,21 @@
 
   //CODE HERE
 
+  function plantTracker() {
+    let plant = 'fern'
+    let height = 12
+    return {
+      readInfo: function() {
+        return `This is a ${plant} plant that is ${height} inches tall.`
+      },
+      waterPlant: function() {
+        return (height += 1)
+      },
+      prunePlant: function() {
+        return (height -= 1)
+      }
+    }
+  }
 
   ////////////PROBLEM 4////////////
 
@@ -80,6 +122,24 @@
 
   //CODE HERE
 
+function inventory() {
+  let products = []
+  return {
+    readProducts: function() {
+      return products
+    },
+    addToProducts: function(string) {
+      return products.push(string)
+    },
+    deleteFromProducts: function(string) {
+      products.map((el, i) => {
+        if (products[i] === string) {
+          products.splice(i, 1)
+        }
+      })
+    }
+  }
+}
 
   /*
     Create a variable called 'shoes' whose value is the invocation of inventory.
@@ -87,6 +147,7 @@
 
   //CODE HERE
 
+  let shoes = inventory()
 
   /*
     Add an item to your shoes array using the addToProducts function
@@ -94,3 +155,4 @@
 
   //CODE HERE
 
+  shoes.addToProducts('item')
